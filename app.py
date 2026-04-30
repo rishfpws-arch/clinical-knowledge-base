487af962eddb68f999c7f74a8bdeadb7281643e9
rishfpws-arch <rishfpws-arch@users.noreply.github.com>
2026-04-30 17:03:41 +0900

    auto: 2026-04-30 17:03:41

--- app.py
+++ app.py
@@ -7415,7 +7415,7 @@ def page_screenshot_gallery():
 
     def _fetch(e):
         try:
-            return download_thumbnail(service, e["fid"])
+            return download_thumbnail(service, e["fid"], max_px=800, quality=88)
         except Exception:
             return None
 
